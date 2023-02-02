6747c216518d9b2a7f82684ee89cd75dd19223c7
Caesar0303 <galaaramaa@gmail.com>
2023-02-02 19:23:16 +0600

    contact

--- index.php
+++ index.php
@@ -9,14 +9,149 @@
 <body>
     <?php 
     $contacts = [
-        'Name' => 'Galym',
-        'Number' => '27637',
-        'Job' => 'IT'
+        [
+            "Name" => "Galym" ,
+            "Number" => "27637",
+            "Job" => 'IT'
+        ],
+        [
+            "Name" => "Alisher" ,
+            "Number" => "1232",
+            "Job" => 'CSSE'
+        ],
+        [
+            "Name" => "Nursultan" ,
+            "Number" => "8522",
+            "Job" => 'IS'
+        ],
+        [
+            "Name" => "Ali" ,
+            "Number" => "2134",
+            "Job" => 'IS'
+        ]
     ];
-    foreach ($contacts as $key => $item) {
-       echo $key . '<br>'; 
-       echo $item . '<br>' . '<br>';
-    }
+
+
     ?>
+
+    <table>
+        <tbody>
+            <tr>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Number
+                </th>
+                <th>
+                    Job
+                </th>
+            </tr>
+
+            <?php
+                foreach ($contacts as $key => $item) {
+
+
+            ?>
+
+            <tr>
+                <td><?php echo $contacts[$key]["Name"] ?></td>
+                <td><?php echo $contacts[$key]["Number"] ?></td>
+                <td><?php echo $contacts[$key]["Job"] ?></td>
+            </tr>
+
+            <?php 
+                }
+            ?>
+
+        </tbody>
+    </table>
+    
+    <table>
+        <tbody>
+            <tr>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Number
+                </th>
+                <th>
+                    Job
+                </th>
+            </tr>
+
+            <?php
+                foreach ($contacts as $key => $item) {
+            ?>
+
+            <tr>
+                <td><?php echo $item["Name"] ?></td>
+                <td><?php echo $item["Number"] ?></td>
+                <td><?php echo $item["Job"] ?></td>
+            </tr>
+
+            <?php 
+                }
+            ?>
+            
+        </tbody>
+    </table>
+
+    <table>
+        <tbody>
+            <tr>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Number
+                </th>
+                <th>
+                    Job
+                </th>
+            </tr>
+
+            <?php
+                foreach ($contacts as $item) {
+            ?>
+
+            <tr>
+                <td><?= $item["Name"] ?></td>
+                <td><?= $item["Number"] ?></td>
+                <td><?= $item["Job"] ?></td>
+            </tr>
+
+            <?php 
+                }
+            ?>
+            
+        </tbody>
+    </table>
+
+    <table>
+        <tbody>
+            <tr>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Number
+                </th>
+                <th>
+                    Job
+                </th>
+            </tr>
+            <?php foreach ($contacts as $item): ?>
+ 
+                <tr>
+                    <td><?= $item["Name"] ?></td>
+                    <td><?= $item["Number"] ?></td>
+                    <td><?= $item["Job"] ?></td>
+                </tr>
+
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </body>
 </html>
